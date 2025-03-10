6bf6f8f5e0dd3aa9d13f768e254e401578942cb2
lovercilli <100765957+lovercilli@users.noreply.github.com>
2025-03-10 15:01:02 +0100

    Update EchoView-ESP32.ino

--- EchoView-ESP32/EchoView-ESP32.ino
+++ EchoView-ESP32/EchoView-ESP32.ino
@@ -2,8 +2,17 @@
 #include <sstream>
 #include "header.h"
 
-#define MIC_PIN 2           // A1 is GPIO2
+//#define MIC_PIN 2           // A1 is GPIO2
+#define MICROPHONES 1         // Number of microphones
+
+//Constants to define microphones direction
+#define NORTH 0
+#define EAST 1
+#define WEST 2
+#define SOUTH 3
 #define NUM_SAMPLES (4096)  // Number of samples for averaging
+
+int micPins[MICROPHONES];   // Array to match the microphones with their physical pin
 
 int currentClientNumber;
 
@@ -18,24 +27,41 @@ void setup()
     // Print column header for Serial Plotter
     Serial.println("Mic_Avg");
 
+    // micPins initialization
+    micPins[NORTH] = 1;       // SDA - GPIO01
+    micPins[EAST] = 2;        // SCL - GPIO02
+    micPins[WEST] = 4;        // GPIO04
+    micPins[SOUTH] = 6;       // GPIO06
+
     connect2Network();
 }
 
 void loop()
 {
-    long sum = 0;  // Use long to prevent overflow
+    long sum[MICROPHONES];  // Use long to prevent overflow
+    int  avgValues[MICROPHONES];
 
-    // Take 100 samples as quickly as possible
-    for (int i = 0; i < NUM_SAMPLES; i++)
-    {
-        sum += analogRead(MIC_PIN);
+    for(int dir = 0; dir < MICROPHONES; dir++){
+      sum[dir] = 0;  // Initialization
+      // Take 100 samples as quickly as possible
+      for (int i = 0; i < NUM_SAMPLES; i++)
+      {
+          sum[dir] += analogRead(micPins[dir]);
+      }
+      avgValues[dir] = sum[dir] / NUM_SAMPLES;                       // Compute the average
+      //Serial.println(String("Microphone ") + dir + ": " + avgValues[dir]);   // Print the average value for plotting
+      Serial.println(String(analogRead(micPins[dir])));
     }
 
-    // Compute the average
-    int avgValue = sum / NUM_SAMPLES;
-
-    // Print the average value for plotting
-    Serial.println(avgValue);
+    // Selecting the Source Microphone
+    int sourceDir = -1; // Default value
+    int sourceAvg = -1; // Default value
+    for(int dir = 0; dir < MICROPHONES; dir++){
+      if(avgValues[dir] > sourceAvg){
+        sourceAvg = avgValues[dir];
+        sourceDir = dir;
+      }
+    }
 
     //delay(50);  // Adjust delay for smooth plotting
 
@@ -59,8 +85,8 @@ void loop()
         currentClientNumber = webSocket.connectedClients();
     }
 
-    JSONtxt = String("{\"MicrophoneValue\":");
-    JSONtxt.concat(avgValue);
-    JSONtxt.concat("}");
-    webSocket.broadcastTXT(JSONtxt);
+    messageTXT = String(sourceDir);
+    messageTXT.concat("$");
+    messageTXT.concat(String(sourceAvg));
+    webSocket.broadcastTXT(messageTXT);
 }
