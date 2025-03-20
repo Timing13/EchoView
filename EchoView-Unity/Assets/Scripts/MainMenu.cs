d29af5d52363e4c1d7de5dbce4f15d4729dabd79
lovercilli <100765957+lovercilli@users.noreply.github.com>
2025-03-20 14:32:15 +0100

    Update MainMenu.cs

--- EchoView-Unity/Assets/Scripts/MainMenu.cs
+++ EchoView-Unity/Assets/Scripts/MainMenu.cs
@@ -1,24 +1,12 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class MenuScript : MonoBehaviour
 {
-    public void EchoViewChosen()
+
+    public void LoadExperience()
     {
         SceneManager.LoadScene("MainScene");
     }
 
-    public void MinigameChosen()
-    {
-        //Could potentially lead to a second ui choice where users could do something like
-        //customize difficulty or presence of certain simulation elements.
-
-        //For now: 
-        SceneManager.LoadScene("GameScene");
-    }
-
-    public void LoadMenuScene()
-    {
-        SceneManager.LoadScene("MenuScene");
-    }
 }
